ca64c2b891078e7d18045ed07fdbdaa71fff32da
Ignacio <ignacionazar9@gmail.com>
2025-07-14 01:22:30 -0300

    agregue la dependencie de react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import FormularioColor from "./components/FormularioColor"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="container mt-4">
+    <FormularioColor></FormularioColor>
+    </div>
   )
 }
 
---
+++ src/components/ListaColores.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListaColores = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ListaColores;
