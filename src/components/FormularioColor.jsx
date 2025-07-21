030b11404c43ddfbbad63d9890e874163eb3aeb3
Ignacio <ignacionazar9@gmail.com>
2025-07-21 16:38:42 -0300

    agregue colores en localstorage

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Card, Button, Form} from "react-bootstrap"
 import ListaColores from './ListaColores';
 
 
 const FormularioColor = () => {
     const [color, setColor] = useState("")
-    const [colores, setColores] = useState([])
+    const coloresLocalstorage = JSON.parse(localStorage.getItem("listaColores")) || []
+    const [colores, setColores] = useState(coloresLocalstorage)
     
+    useEffect(()=>{
+      localStorage.setItem("listaColores", JSON.stringify(colores))
+    }, [colores])
+
     const handleSubmit = (e)=>{
         e.preventDefault();
         console.log("aqui deberia guardar el color")
@@ -16,21 +21,34 @@ const FormularioColor = () => {
          //limpiar el formulario
          setColor("");
     }
+
+    const borrarColor = (nombreColor) => {
+        const coloresFiltrados = colores.filter((item)=> item!==nombreColor)
+        //actualizar el estado colores
+        setColores(coloresFiltrados)
+    }
     
     return (
         <>
-        <Card>
-      <Card.Header>Administrar colores</Card.Header>
+        <Card className='rounded-0'>
+      <Card.Header style={{height: "60px"}}>Administrar colores</Card.Header>
       <Form onSubmit={handleSubmit}>
-      <Card.Body>
-        <Form.Control type="text" placeholder="Ingrese un color ej: Blue" onChange={(e)=> setColor(e.target.value)} value={color}/>
+      <Card.Body className='d-flex' style={{ backgroundColor: '#e8f4fd' }}>
+        <div className='mx-3'
+        style={{
+          width: "120px",
+          height: "100px",
+          backgroundColor: "#3f0fb5",
+        }}
+      ></div>
+        <Form.Control className='rounded-0 mx-3 mt-2' style={{ height: "50px", width: "75%" }} type="text" placeholder="Ingrese un color ej: Blue" onChange={(e)=> setColor(e.target.value)} value={color}/>
         </Card.Body>
-        <Card.Footer className='d-flex justify-content-end'>
+        <Card.Footer className='d-flex justify-content-end align-items-end' style={{height: "90px"}}>
             <Button className='rounded-0' variant="primary" type='submit'>Guardar</Button>
         </Card.Footer>
         </Form>
     </Card>
-    <ListaColores colores={colores}></ListaColores>
+    <ListaColores colores={colores} borrarColor={borrarColor}></ListaColores>
     </>
     );
 };
